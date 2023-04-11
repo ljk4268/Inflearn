f489945f1bc1a0aa9ab71e28659cdea7f9b7aed0
liv.lee <liv.lee@kakaocorp.com>
2023-04-11 10:14:30 +0900

    test1

--- vue3-posts/src/views/HomeView.vue
+++ vue3-posts/src/views/HomeView.vue
@@ -2,7 +2,7 @@
 	<div>
 		<h2>Home View</h2>
 		<p>{{ $route.path }}</p>
-		<button class="btn btn-primary" @click="goAboutPage">About으로 이동???</button>
+		<button class="btn btn-primary" @click="goAboutPage">About으로 이동???@@@</button>
 	</div>
 </template>
 
